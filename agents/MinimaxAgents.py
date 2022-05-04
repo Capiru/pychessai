83506fa5ce6d6acca65022594b898a660a8af3ad
capiru <gabriel_s_prado@hotmail.com>
2022-05-04 11:42:18 +0200

    added minimax policy agent

--- agents/MinimaxAgents.py
+++ agents/MinimaxAgents.py
@@ -1,6 +1,6 @@
 import chess as ch
 from match import match
-from move_choice import minimax,minimax_with_pruning
+from move_choice import minimax,minimax_with_pruning,minimax_with_pruning_and_policyeval
 
 class MinimaxPruningAgent(object):
     def __init__(self,depth = 3,board = ch.Board(),is_white = True):
@@ -11,6 +11,17 @@ class MinimaxPruningAgent(object):
     def choose_move(self,fen):
         self.board = ch.Board(fen)
         score,move = minimax_with_pruning(self.board,self.depth,self.is_white)
+        return [move]
+
+class MinimaxPruningSimplePolicyAgent(object):
+    def __init__(self,depth = 3,board = ch.Board(),is_white = True):
+        self.elo = 400
+        self.depth = depth
+        self.board = board
+        self.is_white = is_white
+    def choose_move(self,fen):
+        self.board = ch.Board(fen)
+        score,move = minimax_with_pruning_and_policyeval(self.board,self.depth,self.is_white)
         return [move]
 
 class MinimaxAgent(object):
