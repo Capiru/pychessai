e8bab52dd0d82756b8153e6d6e0c1b0002bc1743
capiru <gabriel_s_prado@hotmail.com>
2022-06-02 11:54:52 +0200

    added position redundancy

--- agents/MinimaxAgents.py
+++ agents/MinimaxAgents.py
@@ -1,6 +1,6 @@
 import chess as ch
 from match import match
-from move_choice import minimax,minimax_with_pruning,minimax_with_pruning_and_policyeval
+from move_choice import *
 
 class MinimaxPruningAgent(object):
     def __init__(self,depth = 3,board = ch.Board(),is_white = True):
@@ -26,6 +26,21 @@ class MinimaxPruningSimplePolicyAgent(object):
         self.board = board
         score,move,positions = minimax_with_pruning_and_policyeval(self.board,self.depth,self.is_white)
         self.positions += positions
+        return [move]
+
+class MinimaxPruningPositionRedundancyAgent(object):
+    def __init__(self,depth = 3,board = ch.Board(),is_white = True):
+        self.elo = 400
+        self.depth = depth
+        self.board = board
+        self.is_white = is_white
+        self.positions = 0
+        self.positions_analysed = {}
+    def choose_move(self,board):
+        self.board = board
+        score,move,positions,positions_analysed = minimax_with_pruning_policyeval_positionredundancy(self.board,self.depth,self.is_white)
+        self.positions += positions
+        self.positions_analysed.update(positions_analysed)
         return [move]
 
 class MinimaxAgent(object):
--- move_choice.py
+++ move_choice.py
@@ -130,6 +130,58 @@ def minimax_with_pruning_and_policyeval(board,depth,is_player,alpha=-np.inf,beta
                 break
         return min_eval, best_move,positions
 
+def minimax_with_pruning_policyeval_positionredundancy(board,depth,is_player,alpha=-np.inf,beta=np.inf,value_agent = None,policy_model = None,positions=0,positions_analysed={}):
+    ### depth 1 - 21 positions - time 0.004315
+    ### depth 2 - 76 positions - time 0.033392
+    ### depth 3 - 687 positions - time 0.172937
+    ### depth 4 - 4007 positions - time 1.278452
+    ### depth 5 - 30086 positions - time 7.623218
+    ### depth 6 - 82579 positions - time 60.89466
+    positions += 1
+    if depth==0 or board.is_game_over():
+        #this might have problems with depth == 1, should probably return  board.pop() (MAYBE)
+        if value_agent is None:
+            return get_board_evaluation(board),None,positions
+        else:
+            return agent.get_board_evaluation(board),None,positions
+    sorted_list = get_sorted_move_list(board,agent = policy_model)
+    if is_player:
+        max_eval = -np.inf
+        best_move = None
+        for move in sorted_list:
+            board.push(move)
+            try:
+                eval = positions_analysed[board.board_fen]
+            except:
+                eval,a,positions,positions_analysed = minimax_with_pruning_policyeval_positionredundancy(board,depth-1,False,alpha,beta,value_agent,policy_model,positions,positions_analysed)
+                positions_analysed[board.board_fen] = eval
+            if eval>= max_eval:
+                max_eval = eval
+                best_move = move
+            board.pop()
+            alpha = max(alpha,eval)
+            if beta <= alpha:
+                break
+        return max_eval, best_move,positions,positions_analysed
+    else:
+        min_eval = np.inf
+        best_move = None
+        for move in sorted_list:
+            board.push(move)
+            try:
+                eval = positions_analysed[board.board_fen]
+            except:
+                eval,a,positions = minimax_with_pruning_policyeval_positionredundancy(board,depth-1,True,alpha,beta,value_agent,policy_model,positions,positions_analysed)
+                positions_analysed[board.board_fen] = eval
+            if eval<= min_eval:
+                min_eval = eval
+                best_move = move
+            board.pop()
+            beta = min(beta,eval)
+            if beta <= alpha:
+                break
+        return min_eval, best_move,positions,positions_analysed
+
 def get_board_evaluation(board):
     ### Execution time: 0.000453
     if board.is_game_over():
