e321128a7f110f520e919df152879cce57d45528
capiru <gabriel_s_prado@hotmail.com>
2022-06-11 07:56:48 +0200

    added alphabetamaxdepth agent

--- agents/MinimaxAgents.py
+++ agents/MinimaxAgents.py
@@ -43,6 +43,21 @@ class MinimaxPruningPositionRedundancyAgent(object):
         self.positions_analysed.update(positions_analysed)
         return [move]
 
+class AlphaBetaMaxDepth(object):
+    def __init__(self,depth = 3,board = ch.Board(),is_white = True):
+        self.elo = 400
+        self.depth = depth
+        self.board = board
+        self.is_white = is_white
+        self.positions = 0
+        self.positions_analysed = {}
+    def choose_move(self,board):
+        self.board = board
+        score,move,positions,positions_analysed = alphabeta_maxdepth(self.board,self.depth,self.is_white,positions_analysed=self.positions_analysed)
+        self.positions += positions
+        self.positions_analysed.update(positions_analysed)
+        return [move]
+
 class MinimaxAgent(object):
     def __init__(self,depth = 3,board = ch.Board(),is_white = True):
         self.elo = 400
