2aa8cec96ee808174f3f78d3639d44ccea621a3a
capiru <gabriel_s_prado@hotmail.com>
2022-06-11 09:44:09 +0200

    added is white

--- match.py
+++ match.py
@@ -98,7 +98,7 @@ def experiments(agent_one,agent_two,n=100,is_update_elo=True,start_from_opening 
                 #black win
                 outcomes[2] += 1
         if progress_bar:
-            progress.set_description(str(outcomes)+"  1:"+str(agent_one.eval) +"  1-pos:"+str(agent_one.positions)+"   2:"+str(agent_two.eval)+"  2-pos:"+str(agent_two.positions))
+            progress.set_description(str(outcomes)+" is1_white:"+str(int(agent_one.is_white))+"  1:"+str(agent_one.eval) +"  1-pos:"+str(agent_one.positions)+"   2:"+str(agent_two.eval)+"  2-pos:"+str(agent_two.positions))
 
     return outcomes
 
