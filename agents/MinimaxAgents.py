81511735a308def6a6883ace5bdd35613e7531be
capiru <gabriel_s_prado@hotmail.com>
2022-06-14 12:56:27 +0200

    added alphabeta class

--- agents/MinimaxAgents.py
+++ agents/MinimaxAgents.py
@@ -50,18 +50,26 @@ class MinimaxPruningPositionRedundancyAgent(object):
         return [move]
 
 class AlphaBetaMaxDepth(object):
-    def __init__(self,depth = 3,board = ch.Board(),is_white = True):
+    def __init__(self,depth = 3,board = ch.Board(),is_white = True,time_based = False,time_limit = 1):
         self.elo = 400
         self.depth = depth
         self.board = board
         self.is_white = is_white
         self.positions = 0
         self.positions_analysed = {}
+        self.eval = 0
+        self.time_based = time_based
+        self.time_limit = time_limit
+        self.search_obj = AlphaBetaPruning(eval_fun = get_rival_board_evaluation,pruning = True,transposition_table = True,time_based = self.time_based,time_limit = self.time_limit,capture_eval_correction = True)
     def choose_move(self,board):
         self.board = board
-        score,move,positions,positions_analysed = alphabeta_maxdepth(self.board,self.depth,self.is_white,positions_analysed=self.positions_analysed)
+        if self.time_based:
+            score,move,positions,positions_analysed = self.search_obj.time_limit_search(self.board,self.is_white,positions_analysed=self.positions_analysed)
+        else:
+            score,move,positions,positions_analysed = self.search_obj.search(self.board,self.depth,self.is_white,positions_analysed=self.positions_analysed)
         self.positions += positions
         self.positions_analysed.update(positions_analysed)
+        self.eval = score
         return [move]
 
 class MinimaxAgent(object):
--- move_choice.py
+++ move_choice.py
@@ -1,6 +1,155 @@
 import numpy as np
 import chess as ch
 
+class AlphaBetaPruning(object):
+    def __init__(self,depth = 3,eval_fun = get_board_evaluation,policy_fun=get_sorted_move_list,pruning = False,transposition_table = False,time_based = False,time_limit = 1,capture_eval_correction = False):
+        self.depth = depth
+        self.eval_fun = eval_fun
+        self.policy_fun = policy_fun
+        self.pruning = pruning
+        self.transposition_table = transposition_table
+        self.time_based = time_based
+        self.time_limit = time_limit
+        self.start_time = -1
+        self.capture_eval_correction = capture_eval_correction
+        self.max_quiescence_depth = 5
+        if self.time_based:
+            self.search_fun = self.time_limit_search
+        else:
+            self.search_fun = self.search
+
+    def time_limit_search(self,board,is_white = True,alpha = -np.inf,beta = np.inf,positions = 0,positions_analysed = {}):
+        self.start_time = time.process_time()
+        depth = 1
+        is_player = is_white
+        max_score,min_score = -np.inf,np.inf
+        all_pos = positions
+        best_move = None
+        all_analysed_pos = positions_analysed
+        while time.process_time()-self.start_time <= self.time_limit:
+            eval,move,positions,positions_analysed = self.search(board,depth,is_player,alpha,beta,0,all_analysed_pos)
+            all_pos += positions
+            all_analysed_pos.update(positions_analysed)
+            if is_white:
+                if eval >= max_score:
+                    max_score = eval
+                    best_move = move
+            else:
+                if eval <= min_score:
+                    min_score = eval
+                    best_move = move
+            depth += 1
+        if is_white:
+            return max_score,best_move,all_pos,all_analysed_pos
+        else:
+            return min_score,best_move,all_pos,all_analysed_pos
+
+    def quiescence(self,board,depth,is_white = True,alpha = -np.inf,beta = np.inf,positions = 0,positions_analysed = {}):
+        try:
+            eval,a = positions_analysed[board.shredder_fen()+"0"]
+            return eval,a,positions,positions_analysed
+        except:
+            positions += 0
+        curr_score = self.eval_fun(board)
+        capture_list = get_sorted_move_list(board,only_attacks=True)
+        num_captures = len(capture_list)
+        if num_captures == 0:
+            positions_analysed[board.shredder_fen()+"0"] = (curr_score,board.move_stack[-1])
+            return curr_score,None,positions,positions_analysed
+        if depth == 1:
+            alpha = curr_score
+        if is_white:
+            for move in capture_list:
+                board.push(move)
+                positions += 1
+                eval,a,positions,positions_analysed = self.quiescence(board,depth+1,not is_white,alpha,beta,positions,positions_analysed)
+                board.pop()
+                alpha = max(alpha,eval)
+                if beta <= alpha:
+                    break
+                if eval > alpha:
+                    alpha = eval
+            return alpha,None,positions,positions_analysed
+        else:
+            for move in capture_list:
+                board.push(move)
+                positions += 1
+                eval,a,positions,positions_analysed = self.quiescence(board,depth+1,not is_white,alpha,beta,positions,positions_analysed)
+                board.pop()
+                beta = min(beta,eval)
+                if beta <= alpha:
+                    break
+            return beta,None,positions,positions_analysed
+
+       
+
+    def search(self,board,depth = 3,is_white = True,alpha = -np.inf,beta = np.inf,positions = 0,positions_analysed = {}):
+        try:
+            eval,move = positions_analysed[board.shredder_fen()+str(depth)]
+            return eval,move,positions,positions_analysed
+        except:
+            positions += 1
+        if depth==0 or board.is_game_over():
+            if self.capture_eval_correction:
+                return self.quiescence(board,1,not is_white,-np.inf,np.inf,positions,positions_analysed)
+            else:
+                return self.eval_fun(board),None,positions,positions_analysed
+            
+        sorted_list = get_sorted_move_list(board)
+        best_move = None
+        if is_white:
+            max_eval = -np.inf
+            for move in sorted_list:
+                board.push(move)
+                if self.transposition_table:
+                    try:
+                        eval,a = positions_analysed[board.shredder_fen()+str(depth-1)]
+                    except:
+                        eval,a,positions,positions_analysed = self.search(board,depth-1,False,alpha,beta,positions,positions_analysed)
+                        positions_analysed[board.shredder_fen()+str(depth-1)] = (eval,move)
+                else:
+                    eval,a,positions,positions_analysed = self.search(board,depth-1,False,alpha,beta,positions,positions_analysed)
+                if eval >= max_eval:
+                    max_eval = eval
+                    best_move = move
+                board.pop()
+                if self.pruning:
+                    alpha = max(alpha,eval)
+                    if beta <= alpha:
+                        break
+                if self.time_based:
+                    if time.process_time()-self.start_time >= self.time_limit:
+                        break
+            if self.transposition_table:
+                positions_analysed[board.shredder_fen()+str(depth)] = (max_eval,best_move)                    
+            return max_eval, best_move,positions,positions_analysed
+        else:
+            min_eval = np.inf
+            for move in sorted_list:
+                board.push(move)
+                if self.transposition_table:
+                    try:
+                        eval,a = positions_analysed[board.shredder_fen()+str(depth-1)]
+                    except:
+                        eval,a,positions,positions_analysed = self.search(board,depth-1,True,alpha,beta,positions,positions_analysed)
+                        positions_analysed[board.shredder_fen()+str(depth-1)] = (eval,move)
+                else:
+                    eval,a,positions,positions_analysed = self.search(board,depth-1,True,alpha,beta,positions,positions_analysed)
+                if eval<= min_eval:
+                    min_eval = eval
+                    best_move = move
+                board.pop()
+                if self.pruning:
+                    beta = min(beta,eval)
+                    if beta <= alpha:
+                        break
+                if self.time_based:
+                    if time.process_time()-self.start_time >= self.time_limit:
+                        break
+            if self.transposition_table:
+                positions_analysed[board.shredder_fen()+str(depth)] = (min_eval,best_move) 
+            return min_eval, best_move,positions,positions_analysed
+
 def random_choice(possible_moves , probability_map , exploration_size):
     if probability_map is None:
         probability_map = [1/len(possible_moves) for x in range(len(possible_moves))]
