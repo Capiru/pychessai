7424ce5477952a6e3ab356157aa273b6cc6e9dd7
capiru <gabriel_s_prado@hotmail.com>
2022-06-23 15:39:19 +0200

    added import

--- search/search.py
+++ search/search.py
@@ -7,6 +7,7 @@ from match import get_board_as_tensor
 from policy import map_moves_to_policy
 import copy
 import math
+from config import CFG
 
 class AlphaBetaPruning(object):
     def __init__(self,depth = 3,eval_fun = get_board_evaluation,policy_fun=get_sorted_move_list,pruning = False,transposition_table = False,time_based = False,time_limit = 1,capture_eval_correction = False):
