0585d6ce7c931835b423ffd516c2cc984133f776
Capiru <gabriel.silva.2@deliveryhero.com>
2024-06-01 09:55:52 +0200

    added base agents class

---
+++ pychessai/agents/agents.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+
+import chess as ch
+
+
+class Agent(ABC):
+    def __init__(self, depth: int, board: ch.Board, is_white: bool) -> None:
+        super().__init__()
+        self.elo = 400
+        self.depth = depth
+        self.board = board
+        self.is_white = is_white
+
+    @abstractmethod
+    def choose_move(self, board: ch.Board) -> ch.Move:
+        raise Exception("You should implement a choose_move method")
+
+    @abstractmethod
+    def update_board(self, board: ch.Board) -> None:
+        raise Exception("You should implement an update_board method")
+
+    @abstractmethod
+    def get_board_evaluation(self) -> float:
+        raise Exception("You should implement a get_board_evaluation method")
+
+
+class TrainableAgent(Agent):
+    def __init__(
+        self,
+        depth: int,
+        board: ch.Board,
+        is_white: bool,
+        training: bool,
+        model_parameters: dict,
+    ) -> None:
+        super().__init__(depth, board, is_white)
+        self.training = training
+        self.model_parameters = model_parameters
+
+    @abstractmethod
+    def setup_model(self) -> None:
+        raise Exception("You should implement a setup_model method")
+
+    @abstractmethod
+    def get_model_name(self) -> str:
+        raise Exception("You should implement a get_model_name method")
